5996ed80eefd520d84d29d7479ca38e76ebcb5fc
Przemyslaw Kabacinski <przemyslaw_kabacinski@epam.com>
2017-09-20 16:33:32 +0200

    [new] new page training

---
+++ src/pages/training/training.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-training',
+  templateUrl: 'training.html'
+})
+export class TrainingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
